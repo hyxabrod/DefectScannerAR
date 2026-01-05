afb4470b36830dfe5585abb8f94628bf6d4066d3
hyxabrod <hyxabrod@gmail.com>
2026-01-05 18:37:50 +0200

    Initial Commit

---
+++ DefectScannerAR/DefectScannerARApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefectScannerARApp.swift
+//  DefectScannerAR
+//
+//  Created by Maksim Kolotsai on 05.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct DefectScannerARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
